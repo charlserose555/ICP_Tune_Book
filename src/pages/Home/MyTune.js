76389000e869c61cef81d0796560f2abe82f324f
Bluesky <future0209@gmail.com>
2024-08-16 07:51:45 +0200

    Complete the browse tune

--- src/pages/Home/MyTune.js
+++ src/pages/Home/MyTune.js
@@ -11,10 +11,8 @@ import { UpdateInfo, SetTitle, SetOrgTunes, SetCurrentTune } from '../../store/r
 import ABCJS from "abcjs";
 import 'abcjs/abcjs-audio.css';
 import ReactPaginate from 'react-paginate';
-import { keyInfo, rhythmInfo } from "../../const/variable.js";
-import Select from "react-tailwindcss-select";
 
-function Tunes() {
+function MyTunes() {
     const history = useHistory();
     const dispatch = useDispatch();
     const {user} = useSelector((state) => (state.auth));
@@ -26,10 +24,6 @@ function Tunes() {
     const synth = new ABCJS.synth.CreateSynth();
     const synthControl = new ABCJS.synth.SynthController();
     const perPage = 15;
-
-    const [searchTitle, setSearchTitle] = useState('');
-    const [key, setKey] = useState({ value: "all", label: "All key" });
-    const [rhythm, setRhythm] = useState({ value: "all", label: "All Rhythm" });
 
     useEffect(() => {
         dispatch(SetTitle('Browse Tunes'));
@@ -46,14 +40,17 @@ function Tunes() {
         const agent = new HttpAgent({ host: 'https://ic0.app' });
         const actor = Actor.createActor(idlFactory, { agent,  canisterId: process.env.REACT_APP_CANISTER_ID });
 
-        let res = await actor.filter_tunes(searchTitle, rhythm.value, key.value, currentPage);
+        let res = await actor.get_user_tune_list(user.principal, currentPage);
 
         console.log("res", res);
 
         setTotalPage(res[1]);
 
-        dispatch(SetOrgTunes(res[0]));
-        setOrgTunes(res[0]);
+        let tunes = res[0].map((tune) => {return {name: tune.split('.abc')[0]}});
+        dispatch(SetOrgTunes(tunes));
+        setOrgTunes(tunes);
+
+        console.log(tunes);
 
         synthControl.load("#player", null, {
             displayRestart: true,
@@ -63,7 +60,15 @@ function Tunes() {
         });
 
     }
-    
+
+    useEffect(() => {
+        iniABCJS(currentTuneData);
+    }, [currentTuneData])
+
+    useEffect(() => {
+        getOrgTunes();
+    }, [currentPage]);
+
     const iniABCJS = async (currentTuneData) => {
         const visualObj = ABCJS.renderAbc("tunedata", currentTuneData,  { responsive: "resize" });
 
@@ -78,64 +83,39 @@ function Tunes() {
         const agent = new HttpAgent({ host: 'https://ic0.app' });
         const actor = Actor.createActor(idlFactory, { agent,  canisterId: process.env.REACT_APP_CANISTER_ID });
 
-        const tuneData = await actor.get_original_tune(`${selectedTune.title}`);
+        const tuneData = await actor.get_user_tune(user.principal,`${selectedTune.name}.abc`);
         setCurrentTuneData(tuneData);
 
         dispatch(SetCurrentTune({ origin: true, title: selectedTune.name, data: tuneData}));
         const visualObj = ABCJS.renderAbc("tunedata", tuneData,  { responsive: "resize" });
     }
 
-    useEffect(() => {
-        iniABCJS(currentTuneData);
-    }, [currentTuneData])
-
-    useEffect(() => {
-        getOrgTunes();
-    }, [currentPage, rhythm, key, searchTitle]);
-
     return (
     <>
-      <div className="flex flex-col py-8 font-plus px-10 relative w-full gap-8 text-darkblue-800 h-full">
-        <div className="flex flex-col justify-start w-full gap-8 px-2 md:px-4 lg:px-16">
-            <input className="py-8 pl-4 px-4 rounded-2 w-full font-light focus:border-transparent focus:outline-none" placeholder="Search for tunes" value={searchTitle} onChange={(e) => setSearchTitle(e.target.value)}  style={{
-                border: '1.5px solid #e5e7eb', // Set the border color to blue
-                height: '42px', // Adjust the font size as needed
-            }}/>
-
-            <div className="flex flex-row justify-start w-full gap-4 text-gray-400">
-                <div className="w-40">
-                    <Select
-                        value={rhythm}
-                        onChange={(value) => setRhythm(value)}
-                        options={rhythmInfo}
-                    />
-                </div>
-                <div className="w-40">
-                    <Select
-                        value={key}
-                        onChange={(value) => setKey(value)}
-                        options={keyInfo}
-                    />
-                </div>
-            </div>
-        </div>
-        <div className="w-full h-full flex flex-row pb-8 px-2 md:px-4 lg:px-16 gap-16">
-
+      <div className="flex flex-row py-8 font-plus px-10 relative w-full gap-8 text-darkblue-800 h-full">
+        <div className="w-full h-full flex flex-row py-8 px-2 md:px-4 lg:px-16 gap-16">                 
             <div className="w-full h-full flex flex-col gap-4 overflow-y-auto">
+                {orgTunes.length == 0 && (
+                    <div className="flex rounded-4 bg-white px-4 py-8 flex-row w-full justify-center items-center gap-4 cursor-pointer">  
+                               
+                        <p className="font-plus-bold font-bold text-16">No Data</p>
+                    </div>
+                )}
+
                 {orgTunes.map((tune, index) => (
                     <div className="flex rounded-4 bg-white px-4 py-2 flex-row w-full justify-start items-center gap-4 cursor-pointer" key={index} onClick={() => {onSelectTune(tune)}}>  
                         <img className="rounded-8 w-12 h-12" src="/demo/assets/camera.png"
                             style={{
                                 border: '3px solid #faca15' // Adjust the font size as needed
                         }}/>              
-                        <p className="font-plus-bold font-bold text-16">{tune.title}</p>
+                        <p className="font-plus-bold font-bold text-16">{tune.name}</p>
                     </div>
                 ))}
 
-                <div className="w-full flex flex-col md:flex-row text-gray-400">
+<div className="w-full flex flex-col md:flex-row">
                 {totalPage > perPage ? ( <div style={{alignItems: "center", justifyContent:"end"}} >
                     <ReactPaginate
-                    className="inline-flex text-sm h-8 mt-6 text-gray-400"
+                    className="inline-flex text-sm h-8 mt-6"
                     previousLabel={"Previous"}
                     nextLabel={"Next"}
                     nextLinkClassName="flex items-center justify-center px-3 h-8 ms-0 leading-tight
@@ -149,34 +129,37 @@ function Tunes() {
                     border bg-primary border-white text-white hover:bg-white hover:text-green-450"
                     pageCount={Math.ceil(totalPage / perPage)}
                     pageLinkClassName="flex items-center justify-center px-3 h-8 ms-0 leading-tight
-                    border bg-primary border-white hover:bg-white hover:text-green-450 text-gray-600"
+                    border bg-primary border-white text-white hover:bg-white hover:text-green-450"
                     pageClassName="text-black"
                     marginPagesDisplayed={1}
                     pageRangeDisplayed={2}
                     onPageChange={handlePageChange}
                     containerClassName={"pagination"}
-                    activeLinkClassName="flex items-center justify-center h-8 border border-white hover:bg-blue-100 hover:text-green-450 border-white bg-white text-green-450"
+                    activeLinkClassName="flex items-center justify-center h-8 border border-white hover:bg-blue-100 hover:text-green-450 border-white bg-white text-black"
                     />
                 </div>) : ("")}
                 </div>
             </div>
 
-            <div className="w-full h-full bg-white rounded-4 flex flex-col gap-4 p-8">
-                <div class="flex w-full items-center px-3 gap-3">
-                    <div id="player" class="flex-grow"></div>
-                    <a className="fill-btn-secondary text-11 px-4 py-1 text-white font-medium bg-green-450 rounded-2 cursor-pointer flex flex-row justify-center gap-45 items-center"
-                        style={{textAlign: 'center'}} onClick={() => {history.push("/app/playground")}}>
-                        <p className='text-white font-medium'>Edit</p>
-                    </a>
+            {orgTunes.length > 0 && (
+                <div className="w-full h-full bg-white rounded-4 flex flex-col gap-4 p-8">
+                    <div class="flex w-full items-center px-3 gap-3">
+                        <div id="player" class="flex-grow"></div>
+                        <a className="fill-btn-secondary text-11 px-4 py-1 text-white font-medium bg-green-450 rounded-2 cursor-pointer flex flex-row justify-center gap-45 items-center"
+                            style={{textAlign: 'center'}} onClick={() => {history.push("/app/playground")}}>
+                            <p className='text-white font-medium'>Edit</p>
+                        </a>
+                    </div>
+                    <div className="flex w-full flex-col y-scrollable-tag overflow-y-auto">
+                        <div id="tunedata" className="text-gray-800 flex-grow"></div>
+                    </div>
                 </div>
-                <div className="flex w-full flex-col y-scrollable-tag overflow-y-auto">
-                    <div id="tunedata" className="text-gray-800 flex-grow"></div>
-                </div>
-            </div>
+            )}
+
         </div>
       </div>
     </>
   );
 }
 
-export default Tunes;
+export default MyTunes;
